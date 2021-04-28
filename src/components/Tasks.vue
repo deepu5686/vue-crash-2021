cda1f9841c2411b5f7c119a86b9d2faa47699f15
deepu5686 <deepu5686@gmail.com>
2021-04-28 16:02:05 +0530

    Initial commit

---
+++ src/components/Tasks.vue
@@ -0,0 +1,19 @@
+<template>
+    <div :key="task.id" v-for="task in tasks">
+           <Task @toggle-task="$emit('toggle-task', task.id)" 
+           @delete-task="$emit('delete-task', task.id)" :task="task"/>
+    </div>
+</template>
+
+<script>
+import Task from './Task';
+
+    export default{
+        name: 'Tasks',
+        components: {Task},
+        props: {
+            tasks: Array
+        },
+        emits: ['delete-task', 'toggle-task']
+    }
+</script>
